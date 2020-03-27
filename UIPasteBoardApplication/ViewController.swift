3ae11a7d9a1ffce12d7039a9c4fe75784b6bf5fa
CYa <siavashmf@gmail.com>
2020-03-27 19:29:38 +0430

    Initial Commit

---
+++ UIPasteBoardApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIPasteBoardApplication
+//
+//  Created by Siavash Mirzaei-Fard on 3/27/20.
+//  Copyright © 2020 Navaco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
